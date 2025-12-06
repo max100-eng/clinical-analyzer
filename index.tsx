8b85855144ea269d8f965ec8eb172903045f867c
max100-eng <maxbarbetta100@gmail.com>
2025-12-06 14:44:57 +0100

    Actualizar ai sdk para corregir error de build

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './index.css'; // Si esta línea te da error, bórrala o coméntala con //
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
